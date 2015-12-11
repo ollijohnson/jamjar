68ebc098387d2cef19c593e587ee39c120b0dcc1
Phil Connell <phconnel@cisco.com>
2015-12-11 11:14:08 +0000

    v0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name="jamjar",
-      version="0.3",
+      version="0.4",
       description="Jam target and dependency inspection tool",
       author="Phil Connell, Zoe Kelly, Jonathan Loh, Antony Wallace, Ensoft Ltd",
       author_email="philc@ensoft.co.uk",
