a4de8360a0881cacdf04cca1b1de2f4a5bae9d30
Phil Connell <phconnel@cisco.com>
2015-12-21 09:42:10 +0000

    Propagate return values from overridden onecmd
    Required to make exiting cmd loops work...

--- jamjar/ui.py
+++ jamjar/ui.py
@@ -29,7 +29,7 @@ class _BaseCmd(cmd.Cmd):
 
         """
         try:
-            super().onecmd(line)
+            return super().onecmd(line)
         except KeyboardInterrupt:
             pass
 
